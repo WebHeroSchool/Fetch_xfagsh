6eb774888b91f88c473d224824973eab757ce963
xfagsh <visulina@ya.ru>
2020-10-20 16:41:18 +0300

    123

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const card__container = document.querySelector('.card__container');
 const user = 'xfagsh';
+const date = new Date();
+
+
+const getDate = new Promise((resolve, reject) =>
+  setTimeout(() => date ? resolve(date) : reject('Время неизвестно'), 2000)
+);
+
+
 
 fetch('https://api.github.com/users/' + user)
     .then(
@@ -28,7 +36,6 @@ function createCard(obj) {
     // }
     let bio = (!obj.bio) ? 'Нету bio' : obj.bio;
 
-    
     const text = `
     <div class="card">
         <div class="card__img">
@@ -37,7 +44,24 @@ function createCard(obj) {
         <div class="card__title">${obj.login}</div>
         <div class="card__description">${bio}</div>
         <a href="${obj.html_url}" target="_blank" class="card__link">${obj.html_url}</a>
+        <p>${date}</p>
     </div>
     `;
     card__container.insertAdjacentHTML('beforeend', text);
 }
+
+
+// preloader 3 second
+
+
+let spiner = document.getElementById('spiner');
+
+card__container.style.display = 'none';
+
+function hiddenSpiner() {
+
+ spiner.style.display = 'none';
+ card__container.style.display = 'block';
+}
+
+setTimeout( hiddenSpiner, 3000);
